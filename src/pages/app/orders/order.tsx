426477d9cb641c1d902c46a938a747473ef3114f
flvsantos15 <flvsantos300@gmail.com>
2025-01-12 21:38:58 -0300

    chore: add skeleton for table row

--- src/pages/app/orders/order.tsx
+++ src/pages/app/orders/order.tsx
@@ -15,6 +15,7 @@ import {
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilter } from "./order-table-filter";
 import { Pagination } from "@/components/pagination";
+import { OrderTableSkeleton } from "./order-table-sckeleton";
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -80,6 +81,8 @@ export function Orders() {
               </TableBody>
             </Table>
           </div>
+
+          {isLoadingOrders && <OrderTableSkeleton />}
 
           {result && (
             <Pagination
