adf956c7044eee808ecd053a1d4fc648082a3467
flvsantos15 <flvsantos300@gmail.com>
2025-01-13 23:39:19 -0300

    chore: add setup for test using vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import type { UserConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom'
   }
 })
+
+// as UserConfig & {
+//   test: InlineConfig
+// }
